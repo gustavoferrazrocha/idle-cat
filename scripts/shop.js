d5193355337075aa299b40b5ec63e8f8c1cd95cc
GustavoFerrazRocha <gustavoguusfr@gmail.com>
2023-02-20 13:37:06 -0300

    feat: update shop component

--- scripts/shop.js
+++ scripts/shop.js
@@ -38,30 +38,30 @@ function createShopItem() {
       
       const catItem = document.createElement('div')
       catItem.classList.add('shop__item')
+
+      const catInfo = document.createElement('div')
+      catInfo.classList.add('shop__info')
+      catItem.appendChild(catInfo)
   
       const catName = document.createElement('h1');
       catName.classList.add('shop__name');
       catName.textContent = catData.name;
-      catItem.appendChild(catName);
+      catInfo.appendChild(catName);
   
       const catPrice = document.createElement('h2');
       catPrice.classList.add('shop__price');
       catPrice.textContent = `Price: $${catData.price}`;
-      catItem.appendChild(catPrice);
+      catInfo.appendChild(catPrice);
   
       const catQuantity = document.createElement('h2')
       catQuantity.classList.add('shop__quantity');
-      catQuantity.textContent = `Quantity: ${catData.quantity}`;
+      catQuantity.textContent = `${catData.quantity}`;
       catQuantity.setAttribute("id", cat);
       catItem.appendChild(catQuantity);
     
-      const catButton = document.createElement('button');
-      catButton.classList.add('shop__button');
-      catButton.textContent = 'Buy';
-      catButton.addEventListener('click', () => {
+      catItem.addEventListener('click', () => {
         buyCat(catData);
       });
-      catItem.appendChild(catButton);
     
       shopPainel.appendChild(catItem);
   }
@@ -70,7 +70,7 @@ function createShopItem() {
 function updateShop(){
   for(const cat in cats){
     const catQuantity = document.querySelector(`#${cat}`);
-    catQuantity.textContent = `Quantity: ${cats[cat].quantity}`
+    catQuantity.textContent = `${cats[cat].quantity}`
   }
 }
 
